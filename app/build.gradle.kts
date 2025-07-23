3245311bb336fd8383958044f6e05d67f24ff1d7
Your Name <you@example.com>
2025-07-23 18:09:05 -0400

    add some basic coding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,42 +1,14 @@
 plugins {
    alias(libs.plugins.android.application)
    alias(libs.plugins.kotlin.android)
+   kotlin("kapt")
 }
 
 android {
-   namespace = "com.jaisleen.moviesearchapp"
-   compileSdk = 35
-
-   defaultConfig {
-      applicationId = "com.jaisleen.moviesearchapp"
-      minSdk = 24
-      targetSdk = 35
-      versionCode = 1
-      versionName = "1.0"
-
-      testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-   }
-
-   buildTypes {
-      release {
-         isMinifyEnabled = false
-         proguardFiles(
-            getDefaultProguardFile("proguard-android-optimize.txt"),
-            "proguard-rules.pro"
-         )
-      }
-   }
-   compileOptions {
-      sourceCompatibility = JavaVersion.VERSION_11
-      targetCompatibility = JavaVersion.VERSION_11
-   }
-   kotlinOptions {
-      jvmTarget = "11"
-   }
+   // ... your existing android config ...
 }
 
 dependencies {
-
    implementation(libs.androidx.core.ktx)
    implementation(libs.androidx.appcompat)
    implementation(libs.material)
@@ -45,4 +17,12 @@ dependencies {
    testImplementation(libs.junit)
    androidTestImplementation(libs.androidx.junit)
    androidTestImplementation(libs.androidx.espresso.core)
+
+
+   implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+   implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+   implementation("com.squareup.okhttp3:okhttp:4.11.0")
+   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+   implementation("com.github.bumptech.glide:glide:4.16.0")
+   kapt("com.github.bumptech.glide:compiler:4.16.0")
 }
